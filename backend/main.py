Gary0097/zhiyun-data-studio

feat(agent-dock): 应用内接入默认智能体对话面板（AgentDock + SKILL 问数）

backend/main.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 import sys
 import sqlite3
 from pathlib import Path
-from typing import Any
+from typing import Any, AsyncGenerator
 
 from fastapi import APIRouter, File, HTTPException, Response, UploadFile
 from pydantic import BaseModel, Field
@@ -35,6 +35,10 @@ except ImportError:
     from insight_workflow import InsightWorkflowStore
 
 router = APIRouter()
+import httpx
+import json
+from uuid import uuid4
+from fastapi.responses import StreamingResponse
 MAX_UPLOAD = 20 * 1024 * 1024
 PLUGIN_VERSION = "0.9.1"
 
@@ -188,6 +192,108 @@ def create_order_daily_brief(orders: list[dict[str, Any]]) -> dict[str, Any]:
 def analyze_cross_department_metrics(records: list[dict[str, Any]], mapping: dict[str, str]) -> dict[str, Any]:
     """Calculate configurable department output, labor, cost and loss indicators."""
     return analyze_department_metrics(records, mapping)
+
+# ==== 默认智能体接入（AgentDock / Skill 问数） ====
+CONSOLE_CHAT_URL = "http://127.0.0.1:8088/api/console/chat"
+CHAT_TIMEOUT_SECONDS = 300
+DEFAULT_AGENT_ID = "business_analyst"
+
+APP_CONTEXT = (
+"你是「智云 AI OS」企业数据分析中心的智能体助手。你可以调用 `analyze_order_delivery_risk`、`analyze_cross_department_metrics`、`create_order_daily_brief`、`analyze_order_kpi_trends` 等工具，基于真实 Data Core 数据回答订单交付风险、跨部门人效、每日简报和趋势问题。当用户询问订单风险、部门指标、经营简报或趋势时，请先调用对应工具再给出结论；不要凭空编造数据。"
+)
+
+
+class AgentChatRequest(BaseModel):
+    """Client payload for the streaming in-app agent chat."""
+
+    text: str = Field(min_length=1, max_length=4000, description="User message")
+    session_id: str | None = Field(default=None, description="Persistent conversation id")
+    user_id: str | None = Field(default="default", description="Calling user id")
+    app_id: str | None = Field(default="zhiyun-data-studio")
+    context: str | None = Field(default=None, description="Optional system context")
+    history: list[dict[str, Any]] = Field(
+        default_factory=list,
+        description="Prior turns [{role, text}] for multi-turn context",
+    )
+
+
+def _build_input(body: AgentChatRequest) -> list[dict[str, Any]]:
+    """Build the console ``input`` message list from the dock payload."""
+    context = body.context or APP_CONTEXT
+    input_messages: list[dict[str, Any]] = []
+    if context:
+        input_messages.append(
+            {"role": "system", "content": [{"type": "text", "text": context}]}
+        )
+    for turn in body.history:
+        if not isinstance(turn, dict):
+            continue
+        role = turn.get("role")
+        text = turn.get("text")
+        if not isinstance(text, str) or not text.strip():
+            continue
+        mapped_role = "assistant" if role in ("bot", "assistant") else "user"
+        input_messages.append(
+            {"role": mapped_role, "content": [{"type": "text", "text": text}]}
+        )
+    input_messages.append(
+        {"role": "user", "content": [{"type": "text", "text": body.text}]}
+    )
+    return input_messages
+
+
+@router.post("/agent/chat")
+async def agent_chat(body: AgentChatRequest) -> StreamingResponse:
+    """Proxy a user message to the real console chat and stream its SSE reply."""
+    session_id = body.session_id or f"zhiyun-data-studio-{uuid4().hex}"
+    user_id = body.user_id or "default"
+
+    payload = {
+        "input": _build_input(body),
+        "session_id": session_id,
+        "user_id": user_id,
+        "stream": True,
+        "metadata": {
+            "app_id": body.app_id or "zhiyun-data-studio",
+            "source_kind": "agent_dock",
+            "data_mode": "real",
+        },
+    }
+
+    async def event_generator() -> AsyncGenerator[str, None]:
+        try:
+            async with httpx.AsyncClient(timeout=CHAT_TIMEOUT_SECONDS) as client:
+                async with client.stream(
+                    "POST",
+                    CONSOLE_CHAT_URL,
+                    json=payload,
+                    headers={"X-Agent-Id": DEFAULT_AGENT_ID},
+                ) as response:
+                    if response.status_code != 200:
+                        err_body = await response.aread()
+                        text = err_body.decode("utf-8", errors="replace")
+                        yield f"data: {json.dumps({'error': text})}\n\n"
+                        return
+                    async for line in response.aiter_lines():
+                        if line == "":
+                            yield "\n"
+                        else:
+                            yield line + "\n"
+        except httpx.TimeoutException:
+            yield f"data: {json.dumps({'error': '智能体响应超时，请稍后重试'})}\n\n"
+        except Exception as exc:  # pragma: no cover - defensive
+            yield f"data: {json.dumps({'error': f'调用智能体失败: {exc}'})}\n\n"
+
+    return StreamingResponse(
+        event_generator(),
+        media_type="text/event-stream",
+        headers={
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "X-Accel-Buffering": "no",
+        },
+    )
+
 
 
 class DataStudioPlugin:

Gary0097/zhiyun-finance-studio

backend/main.py
@@ -7,7 +7,7 @@ import json
 import sys
 import sqlite3
 from pathlib import Path
-from typing import Any
+from typing import Any, AsyncGenerator
 
 from fastapi import APIRouter, HTTPException, Response
 from pydantic import BaseModel, Field
@@ -24,6 +24,9 @@ except ImportError:
     from finance_workflow import FinanceWorkflowStore
 
 router = APIRouter()
+import httpx
+from uuid import uuid4
+from fastapi.responses import StreamingResponse
 PLUGIN_VERSION = "0.2.0"
 
 
@@ -168,6 +171,108 @@ def forecast_review_cost(parameters: dict[str, Any]) -> dict[str, Any]:
     """Forecast unit cost under real material/BOM price changes and persist a reviewable artifact."""
     payload = forecast_cost(parameters)
     return _store().create_artifact("cost", "成本预测", payload)
+
+# ==== 默认智能体接入（AgentDock / Skill 问数） ====
+CONSOLE_CHAT_URL = "http://127.0.0.1:8088/api/console/chat"
+CHAT_TIMEOUT_SECONDS = 300
+DEFAULT_AGENT_ID = "finance_invoice"
+
+APP_CONTEXT = (
+"你是「智云 AI OS」财务共享中心的智能体助手。你可以调用 `audit_review_expenses`、`run_review_financial_analysis`、`forecast_review_cost` 等工具，基于真实财务数据回答费用审核、财务分析和成本预测问题。当用户询问费用、财务分析或成本时，请先调用对应工具再给出结论；不要凭空编造数据。"
+)
+
+
+class AgentChatRequest(BaseModel):
+    """Client payload for the streaming in-app agent chat."""
+
+    text: str = Field(min_length=1, max_length=4000, description="User message")
+    session_id: str | None = Field(default=None, description="Persistent conversation id")
+    user_id: str | None = Field(default="default", description="Calling user id")
+    app_id: str | None = Field(default="zhiyun-finance-studio")
+    context: str | None = Field(default=None, description="Optional system context")
+    history: list[dict[str, Any]] = Field(
+        default_factory=list,
+        description="Prior turns [{role, text}] for multi-turn context",
+    )
+
+
+def _build_input(body: AgentChatRequest) -> list[dict[str, Any]]:
+    """Build the console ``input`` message list from the dock payload."""
+    context = body.context or APP_CONTEXT
+    input_messages: list[dict[str, Any]] = []
+    if context:
+        input_messages.append(
+            {"role": "system", "content": [{"type": "text", "text": context}]}
+        )
+    for turn in body.history:
+        if not isinstance(turn, dict):
+            continue
+        role = turn.get("role")
+        text = turn.get("text")
+        if not isinstance(text, str) or not text.strip():
+            continue
+        mapped_role = "assistant" if role in ("bot", "assistant") else "user"
+        input_messages.append(
+            {"role": mapped_role, "content": [{"type": "text", "text": text}]}
+        )
+    input_messages.append(
+        {"role": "user", "content": [{"type": "text", "text": body.text}]}
+    )
+    return input_messages
+
+
+@router.post("/agent/chat")
+async def agent_chat(body: AgentChatRequest) -> StreamingResponse:
+    """Proxy a user message to the real console chat and stream its SSE reply."""
+    session_id = body.session_id or f"zhiyun-finance-studio-{uuid4().hex}"
+    user_id = body.user_id or "default"
+
+    payload = {
+        "input": _build_input(body),
+        "session_id": session_id,
+        "user_id": user_id,
+        "stream": True,
+        "metadata": {
+            "app_id": body.app_id or "zhiyun-finance-studio",
+            "source_kind": "agent_dock",
+            "data_mode": "real",
+        },
+    }
+
+    async def event_generator() -> AsyncGenerator[str, None]:
+        try:
+            async with httpx.AsyncClient(timeout=CHAT_TIMEOUT_SECONDS) as client:
+                async with client.stream(
+                    "POST",
+                    CONSOLE_CHAT_URL,
+                    json=payload,
+                    headers={"X-Agent-Id": DEFAULT_AGENT_ID},
+                ) as response:
+                    if response.status_code != 200:
+                        err_body = await response.aread()
+                        text = err_body.decode("utf-8", errors="replace")
+                        yield f"data: {json.dumps({'error': text})}\n\n"
+                        return
+                    async for line in response.aiter_lines():
+                        if line == "":
+                            yield "\n"
+                        else:
+                            yield line + "\n"
+        except httpx.TimeoutException:
+            yield f"data: {json.dumps({'error': '智能体响应超时，请稍后重试'})}\n\n"
+        except Exception as exc:  # pragma: no cover - defensive
+            yield f"data: {json.dumps({'error': f'调用智能体失败: {exc}'})}\n\n"
+
+    return StreamingResponse(
+        event_generator(),
+        media_type="text/event-stream",
+        headers={
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "X-Accel-Buffering": "no",
+        },
+    )
+
 
 
 class FinanceStudioPlugin: